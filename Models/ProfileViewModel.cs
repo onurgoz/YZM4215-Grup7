ebc19b1112f281f367e1665c32dc6f2ba42be200
baha908 <baho908@gmail.com>
2021-01-01 22:05:09 +0300

    Missing Model added

---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+namespace YZM4215_Grup7.Models
+{
+    public class ProfileViewModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+        public string FullName { get; set; }
+    }
+}
